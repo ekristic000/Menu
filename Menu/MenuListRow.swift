65b434fce715a89cefca5df69ac0215ede240c4b
ekristic000 <e16mae@gmail.com>
2025-02-10 09:00:44 -0600

    Updated MenuRowList

---
+++ Menu/MenuListRow.swift
@@ -0,0 +1,18 @@
+//
+//  MenuListRow.swift
+//  Menu
+//
+//  Created by Kristich, Erin M on 2/10/25.
+//
+
+import SwiftUI
+
+struct MenuListRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuListRow()
+}
